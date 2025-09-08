b5c3aaa9a1e4d097e0d3801761e6c4d9a05c46f9
ProgramNS <blise01@hotmail.com>
2025-09-08 02:50:41 +0000

    remove sensitive file

--- .env.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('GOOGLE_CLIENT_ID', "1023579101839-o2u4u9n4dhe9bvfi3ik2r2og3s9mmeoj.apps.googleusercontent.com");
-define('GOOGLE_CLIENT_SECRET', "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");
-define('GOOGLE_REDIRECT_URI', "https://asokababystore.com/src/auth/google_login_pubs.php"); 
-define('GOOGLE_REFRESH_TOKEN', "1//0gNCeanJqf3ecCgYIARAAGBASNwF-L9IrrIjjMWiDnXxgkc8ay_NnvnBZEOjHsl5RnuVCF23Ww1fjjv_K7rHHuz0lDGHP6Um4gBQ");
-define('GOOGLE_REDIRECT_URI_REFRESH_TOKEN', "https://asokababystore.com/src/api/forget_pass/get_refresh_token.php");
-define('GOOGLE_SENDER_MAIL', "it.prog.asoka@gmail.com");
