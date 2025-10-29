a8c88bf68c101aba3fa49ce9c1e6df9301826f62
ITAsokaProgram <it.prog.asoka@gmail.com>
2025-10-29 16:23:59 +0700

    chore: update kategori trans b after insert data trans b

--- src/api/insert/run_cron.php
+++ src/api/insert/run_cron.php
@@ -2,6 +2,9 @@
 include "insert_data_pembayaran_to_pembayaran_b.php";
 sleep(30);
 include "insert_data_trans_to_trans_b.php";
+
+include __DIR__ . '/../../../cron/update_kategori_trans_b.php';
+
 sleep(30);
 include "delete_data_pembayaran.php";
 sleep(30);
