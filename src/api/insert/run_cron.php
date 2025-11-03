d8731ff73ce1b8f96f94c1697ca2c959900c74f6
ITAsokaProgram <it.prog.asoka@gmail.com>
2025-11-03 09:12:21 +0700

    fix: cron update kategori trans_b

--- src/api/insert/run_cron.php
+++ src/api/insert/run_cron.php
@@ -1,8 +1,6 @@
 <?php
-require_once __DIR__ . '/../../config.php';
-
+require_once __DIR__ . '/../../../config.php';
 include "insert_data_pembayaran_to_pembayaran_b.php";
-sleep(30);
 include "insert_data_trans_to_trans_b.php";
 
 include __DIR__ . '/../../../cron/update_kategori_trans_b.php';
