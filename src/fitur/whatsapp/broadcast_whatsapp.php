e3a18a60f446091a68a11848fb1ef6ab5b94137c
edo6661 <muhammadridho5654@gmail.com>
2026-01-13 11:38:17 +0700

    chore: guard broadcast whatsapp page

--- src/fitur/whatsapp/broadcast_whatsapp.php
+++ src/fitur/whatsapp/broadcast_whatsapp.php
@@ -3,6 +3,9 @@ session_start();
 include '../../../aa_kon_sett.php';
 require_once __DIR__ . '/../../component/menu_handler.php';
 $menuHandler = new MenuHandler('whatsapp_broadcast');
+if (!$menuHandler->initialize()) {
+  exit();
+}
 ?>
 
 <!DOCTYPE html>
