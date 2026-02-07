2064c12fb473c7a9f89300179191e3348740779c
edo6661 <muhammadridho5654@gmail.com>
2026-02-07 09:45:04 +0700

    chore: delete cache git files

--- redis.php
+++
@@ -1,7 +0,0 @@
-<?php
-$env =  parse_ini_file('.env');
-$redis = new Redis();
-$redis->connect($env['REDIS_CONNECTION'], 6379);
-if (!empty($env['REDIS_PASSWORD'])) {
-    $redis->auth($env['REDIS_PASSWORD']);
-}
